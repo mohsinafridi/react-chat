b57fd9e8ea24fca69436398bce3daa4a2f24a816
moaz <mohsin.afridi91@gmail.com>
2020-07-10 22:52:13 +0500

    initialapp

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+import Chat from './Chat';
+
+function App() {
+  return(
+    <div style={{ margin: '0 30%' }}>
+    <Chat />
+  </div>
+  )
+}
+
+export default App;
